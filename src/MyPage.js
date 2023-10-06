0a334427dc548ecbfb7259e51c56b3b32507aaef
hchaehyun <hchaehyun@naver.com>
2023-10-06 20:35:40 +0900

    로그아웃 서버 연동 및 구현

--- src/MyPage.js
+++ src/MyPage.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import axiosInstance from './AxiosInstance';
 
 function MyPage({ user, onLogout }) {
   console.log(user);
   const navigate = useNavigate();
 
-
   // 페이지 이동 핸들러 함수 정의
   // 반응형 고려하면 별도의 핸들러 함수를 정의하는 방식이 더 유용하고 확장성 좋음
 
-  const handleLogout = () => {
-    onLogout();  // App.js의 handleLogout 함수를 호출하여 상태를 초기화
-    navigate('/'); // 로그아웃 로직을 처리한 후, 루트 홈페이지(Login)로 이동
+  const handleLogoutRequest = async () => {
+    try {
+      const response = await axiosInstance.post('api/auth/logout'); // 로그아웃 API 경로 추가/수정
+      
+      if (response.data && response.data.AccessToken !== undefined) {
+        localStorage.setItem('AccessToken', response.data.AccessToken); // 빈 값 저장
+        // 또는 localStorage.removeItem('AccessToken'); 로 토큰을 삭제
+      }
+      
+      onLogout();  // App.js의 handleLogout 함수를 호출하여 상태를 초기화
+      navigate('/'); // 로그아웃 후, 루트 홈페이지(Login)로 이동
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        // 401 에러 무시하고 로그아웃 처리
+        onLogout();
+        navigate('/');
+      } else {
+        alert('로그아웃 중 오류가 발생했습니다.');
+      }
+    }
   };
 
   const handleChangePasswordClick = () => {
@@ -25,14 +42,12 @@ function MyPage({ user, onLogout }) {
     navigate('/choosestage');
   };
 
-
-
   return (
     <div className="my-page-container">
       <h2>Welcome, {user}!</h2>  {/* 추후 닉네임 받아오는 것으로 변경 */}
       <p>Your User ID: {user}</p>
 
-      <Button variant="contained" color="primary" onClick={handleLogout}>
+      <Button variant="contained" color="primary" onClick={handleLogoutRequest}>
         로그아웃
       </Button>
 
