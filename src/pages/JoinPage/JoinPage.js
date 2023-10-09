9026db9ffdcb8a30c3e93366ab6a45f286bdd414
hchaehyun <hchaehyun@naver.com>
2023-10-09 15:08:48 +0900

    join 수정

--- src/pages/JoinPage/JoinPage.js
+++ src/pages/JoinPage/JoinPage.js
@@ -10,56 +10,192 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Alert from "@mui/material/Alert";
 import Stack from "@mui/material/Stack";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import axiosInstance from "../../Components/AxiosInstance";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
 
 const defaultTheme = createTheme();
 
 function JoinPage() {
   const navigate = useNavigate();
-  //비밀번호 state
-  const [password, setPassword] = useState("");
-  //비밀번호 유효성 state
+
+  //유저네임, 닉네임, 이메일 중복체크용 state
+  const [usernameChecked, setUsernameChecked] = useState(false);
+  const [nicknameChecked, setNicknameChecked] = useState(false);
+  const [emailChecked, setEmailChecked] = useState(false);
+
+  const handleTempChange = (event) => {
+    const { name, value } = event.target;
+    setTempValues((prevState) => ({ ...prevState, [name]: value }));
+
+    // 값이 변경되면 상태를 초기화
+    switch (name) {
+      case "username":
+        setUsernameChecked(false);
+        break;
+      case "nickname":
+        setNicknameChecked(false);
+        break;
+      case "email":
+        setEmailChecked(false);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const checkDuplicate = (type) => {
+    const regex = /^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]+$/;
+
+    if (!tempValues[type]) {
+      alert(`${fieldDisplayNames[type]}를 입력해주세요.`);
+      return;
+    } else if (!regex.test(tempValues[type])) {
+      alert(`알파벳, 숫자, 특수문자만 입력해주세요.`);
+      return;
+    }
+
+    axiosInstance
+      .post(`api/users/valid/${type}`, { [type]: tempValues[type] })
+      .then((response) => {
+        if (response.status === 200) {
+          switch (type) {
+            case "username":
+              setUsernameChecked(true);
+              break;
+            case "nickname":
+              setNicknameChecked(true);
+              break;
+            case "email":
+              setEmailChecked(true);
+              break;
+            default:
+              break;
+          }
+        } else {
+          alert("서버 응답 오류. 다시 시도해주세요.");
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 400) {
+          alert("중복되었습니다.");
+        } else {
+          alert("서버 응답 오류. 다시 시도해주세요.");
+        }
+      });
+  };
+
+  const fieldDisplayNames = {
+    username: "아이디를",
+    nickname: "닉네임을",
+    email: "이메일을",
+  };
+
+  const iconButtonStyles = {
+    width: 100,
+    mr: 1,
+    backgroundColor: "white",
+    fontSize: "16px",
+    borderRadius: "3px",
+    Animation: "none",
+    "&:hover": {
+      mr: 1,
+      borderRadius: "3px",
+      // border: "0px solid dodgerblue",
+    },
+  };
+
+  //이메일 유효성 체크
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  const [isValidEmail, setIsValidEmail] = useState(false);
+
+  const handleEmailChange = (event) => {
+    const emailValue = event.target.value;
+    setTempValues((prevState) => ({ ...prevState, email: emailValue }));
+    setIsValidEmail(emailRegex.test(emailValue));
+  };
+
+  //////////여기까지 유저네임, 닉네임, 이메일
+
+  const [buttonState, setButtonState] = useState({
+    color: "default", //
+    text: "중복확인", //
+  });
+
+  //유저네임, 닉네임, 이메일 state
+  const [upperInfo, setUpperInfo] = useState({
+    username: "",
+    nickname: "",
+    email: "",
+  });
+
+  //사용자에게 보이게하는부분
+  const [tempValues, setTempValues] = useState({
+    username: "",
+    nickname: "",
+    email: "",
+  });
+
+  const handleFinalizeInput = (event) => {
+    const { name, value } = event.target;
+    setUpperInfo((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  //비밀번호관련
+  const [password, setPassword] = useState(""); //비밀번호 state
   const [passwordState, setPasswordState] = useState(
     "8자리 이상, 알파벳과 숫자만 입력 가능.(특수문자 제외)"
-  );
-  //비밀번호 재확인 state
+  ); //비밀번호 유효성 state
   const [passwordConfirmState, setPasswordConfirmState] = useState(
     "동일한 비밀번호를 입력하여 주세요."
-  );
-  //가입버튼 활성화 관리 state
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-  //가입확인 알러트 state
-  const [showAlert, setShowAlert] = useState(false);
+  ); //비밀번호 재확인 state
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true); //가입버튼 활성화 관리 state
+
+  //가입 완료 알러트 state
+  const [showJoinAlert, setShowJoinAlert] = useState(false);
 
   //가입완료 서밋 함수
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const userName = data.get("userName");
-    const nickName = data.get("nickName");
+    const username = data.get("username");
+    const nickname = data.get("nickname");
     const email = data.get("email");
     const password = data.get("password");
 
     console.log({
-      userName: data.get("userName"),
-      nickName: data.get("nickName"),
+      username: data.get("username"),
+      nickname: data.get("nickname"),
       email: data.get("email"),
       password: data.get("password"),
-      passwordConfirm: data.get("passwordConfirm"),
     });
 
-    //가입 완료 메시지 알러트
-    setShowAlert(true);
-    // 알림 표시 후 3초 후에 메인화면으로 이동
-    setTimeout(() => {
-      navigate("/");
-    }, 3000);
+    //조건문 달아서 가입완료여부 확인하고 가입완료 -> 가입 불가 나누기
+    axiosInstance
+      .post("api/users", { username, nickname, email, password })
+      .then((response) => {
+        //응답이 200이면 회원가입, 로그인페이지로 이동
+        if (response.status === 200) {
+          //가입 완료 메시지 알러트
+          setShowJoinAlert(true);
+          // 알림 표시 후 3초 후에 메인화면으로 이동
+          setTimeout(() => {
+            navigate("/");
+          }, 3000);
+        } else {
+          alert("회원가입실패");
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 400) {
+          alert("잘못된 정보를 입력하였습니다.");
+        } else {
+          alert("문제가 발생하였습니다.");
+        }
+      });
   };
 
   //비밀번호 유효성 검사함수
@@ -131,7 +267,7 @@ function JoinPage() {
               <AssignmentIndIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              회원 정보를 입력하여 주세요.
+              JOINING PAGE
             </Typography>
             <Box
               component="form"
@@ -145,20 +281,55 @@ function JoinPage() {
                 <TextField
                   margin="dense"
                   autoComplete="name"
-                  name="userName"
+                  name="username"
                   required
                   fullWidth
-                  id="userName"
-                  label="User Name"
+                  id="username"
+                  label="User ID"
+                  value={tempValues.username}
+                  onChange={handleTempChange}
+                  onBlur={handleFinalizeInput}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          sx={iconButtonStyles}
+                          edge="end"
+                          color={usernameChecked ? "primary" : "default"}
+                          onClick={() => checkDuplicate("username")}
+                        >
+                          {usernameChecked ? "확인완료!" : "중복확인"}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
                 />
                 <TextField
                   margin="dense"
-                  autoComplete="given-name"
-                  name="firstName"
+                  autoComplete="nickname"
+                  name="nickname"
                   required
                   fullWidth
-                  id="firstName"
+                  id="nickname"
                   label="Nick Name"
+                  value={tempValues.nickname}
+                  onChange={handleTempChange}
+                  onBlur={handleFinalizeInput}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          disableRipple//안먹힘! 클릭효과 안사라짐!
+                          sx={iconButtonStyles}
+                          edge="end"
+                          color={nicknameChecked ? "primary" : "default"}
+                          onClick={() => checkDuplicate("nickname")}
+                        >
+                          {nicknameChecked ? "확인완료!" : "중복확인"}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
                 />
                 <TextField
                   margin="dense"
@@ -168,6 +339,23 @@ function JoinPage() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  value={tempValues.email}
+                  onChange={handleTempChange}
+                  onBlur={handleFinalizeInput}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          sx={iconButtonStyles}
+                          edge="end"
+                          color={emailChecked ? "primary" : "default"}
+                          onClick={() => checkDuplicate("email")}
+                        >
+                          {emailChecked ? "확인완료!" : "중복확인"}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
                 />
               </Box>
 
@@ -237,8 +425,8 @@ function JoinPage() {
         </Grid>
       </Grid>
 
-      {/* 알림 표시 조건 추가 */}
-      {showAlert && (
+      {/* 가입완료알러트 */}
+      {showJoinAlert && (
         <Stack
           sx={{
             width: "100%",
